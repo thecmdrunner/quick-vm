8e5abae260423d3635374a4587dc89a861bb9e0a
Pranav <pranavkulkarni195@gmail.com>
2021-03-15 16:57:40 +0530

    ☕ Improved

--- one-liner.sh
+++ one-liner.sh
@@ -3,7 +3,8 @@
 
 if [[ -f /usr/bin/pacman ]]
 then
-  sudo pacman -S --noconfirm qemu libvirt bridge-utils edk2-ovmf vde2 ebtables dnsmasq openbsd-netcat virt-manager
+  exit 1
+  sudo pacman -S --noconfirm qemu libvirt bridge-utils edk2-ovmf vde2 ebtables dnsmasq openbsd-netcat virt-manager && sudo systemctl enable libvirtd
 elif [[ -f /usr/bin/dnf ]]
 then
   sudo dnf -y install qemu-kvm libvirt bridge-utils virt-install virt-manager
@@ -12,4 +13,5 @@ then
   sudo apt install -y qemu qemu-kvm libvirt-bin libvirt-daemon libvirt-clients bridge-utils virt-manager
 else
   echo "Your System possibly isn't Debian/Fedora/Arch, make sure to install the KVM dependencies before you continue."
+  exit 1
 fi
