a670fcbf977949985c52f9cddcf1ade76ee688a6
Pranav <pranavkulkarni195@gmail.com>
2021-04-08 18:08:02 +0530

    ☕ Improved

--- one-liner.sh
+++ one-liner.sh
@@ -89,7 +89,10 @@ byee() {
 
 cpu_kvm_flags=$(egrep -c '(vmx|svm)' /proc/cpuinfo)
 cpu_vt=$(lscpu | grep Virtualization)
-kvm_pass=$(/usr/bin/virt-host-validate | grep '/dev/kvm exists')
+
+if [[ -f /usr/bin/virt-host-validate ]]; then
+  kvm_pass=$(/usr/bin/virt-host-validate | grep '/dev/kvm exists')
+fi
 
 check_kvm() {
 
